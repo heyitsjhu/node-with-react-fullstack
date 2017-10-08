b488c94966036272a05d28098513fd76abbf1d08
Johnny Hu <heyitsjhu@gmail.com>
2017-10-08 13:51:20 -0700

    Setup boilerplate react and redux setup

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div>
+      Hi there!
+    </div>
+  )
+};
+
+// make App available for import
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+// create a store 
+// first arg, collection of stores
+// second arg, initial state
+// third arg, apply middleware
+const store = createStore(() => [], {}, applyMiddleware());
+
+// first arg: component
+// seconrd arg: destination in public/index.html where component will be rendered
+ReactDOM.render(
+  // <Provider /> the glue that bonds react and redux together, 
+  // making the redux stores accessible to every component in the app
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('#root')
+);
