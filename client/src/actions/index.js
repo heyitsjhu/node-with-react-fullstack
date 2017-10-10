371bd1b9cf9b36b05dd21ffe6bfe31297d0015f3
Johnny Hu <heyitsjhu@gmail.com>
2017-10-09 21:27:10 -0700

    Refactor redux actions and update package.json file in react directory

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,15 +3,16 @@
 // actions typically imported in App.js
 
 // file separation is for organization purposes
-import axios from 'axios';
-import { FETCH_USER } from './types';
+import axios from "axios";
+import { FETCH_USER } from "./types";
 
 // action that fetches a user
-export const fetchUser = () => {
-	// syntax / style via redux-thunk and async/await
-	return function(dispatch) {
-		// axios will return a data object in the returned payload containing the actual response data.
-		axios.get('/api/current_user')
-			.then(res => dispatch({ type: FETCH_USER, payload: res }));
-	};
+// arrow functions, if you only have one expression, you can remove the {} and return keyword.
+// syntax / style via redux-thunk and async/await
+export const fetchUser = () => async dispatch => {
+  // axios will return a data object in the returned payload containing the actual response data.
+  const res = await axios.get("/api/current_user");
+
+  // this is redux-thunk?
+  dispatch({ type: FETCH_USER, payload: res });
 };
