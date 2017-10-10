3b9a7ec31d9df6c39058596f922902c2dc357afb
Johnny Hu <heyitsjhu@gmail.com>
2017-10-09 21:53:39 -0700

    Update header to dynamically display links based on user authentication

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 
+// hook up component to the redux store
+import { connect } from 'react-redux';
+
 class Header extends Component {
+
+  // methods defined inside this component,
+  // but outside of built-in react functions,
+  // are classified as helper methods
+
+  renderContent() {
+    switch (this.props.auth) {
+      // unsure if user is logged in (still pending)
+      case null:
+        return;
+      // certain user is logged out
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      // user is logged in
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
+
   render() {
+    console.log(this.props);
     return(
       <header>
         <nav>
           <div className="nav-wrapper">
             <a href="#" className="left brand-logo">Welcome</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li>
-                <a href="/auth/google">Login with Google</a>
-              </li>
+
+              {this.renderContent()}
+
             </ul>
           </div>
         </nav>
@@ -19,4 +51,15 @@ class Header extends Component {
   }
 }
 
-export default Header;
+// retrieves the entire redux store, allowing us
+// to return just the state we're concerned about
+// for this component
+// check the reducers index.js page to see which properties (states)
+// are available
+// the property (state) is then accessible through this.props
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+// connect this component to redux store
+export default connect(mapStateToProps)(Header);
