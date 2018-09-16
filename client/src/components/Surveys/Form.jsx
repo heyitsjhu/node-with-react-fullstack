4c69b4a171786f09c6bf57bfac7987cd58d38d04
Johnny Hu <jgeehen.hu@gmail.com>
2018-09-15 19:31:49 -0700

    Refactor form code

--- client/src/components/Surveys/Form.jsx
+++ client/src/components/Surveys/Form.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from '../Form/Field';
+
+const FIELDS = [
+  {
+    label: 'Survey Title',
+    name: 'title'
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject'
+  },
+  {
+    label: 'Email Body',
+    name: 'body'
+  },
+  {
+    label: 'Recipient List',
+    name: 'emails'
+  }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    // reduxForm passes Field props to custom componemt
+    return FIELDS.map(({ label, name }) => (
+      <Field
+        key={name}
+        label={label}
+        type="text"
+        name={name}
+        component={SurveyField}
+      />
+    ));
+  }
+
   render() {
     // explain how reduxForm provides a handleSubmit function to the form
     // where you can pass in a custom callback and retrieve the form's data
@@ -9,7 +42,7 @@ class SurveyForm extends Component {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
